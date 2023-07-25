0b03625fdbe4b8772b57f3ef548cc0b350ee533d
Shiv(Workspace) <shivatneosoft@gmail.com>
2023-07-25 12:03:46 +0530

    SumOfSubArray

--- ClosestPair.java
+++ ClosestPair.java
@@ -5,25 +5,18 @@ public class ClosestPair {
     public static void main(String[] args) {
         int[] first =  { 10, 12, 15, 18, 20 };
         int[] second = { 1, 4, 6, 8 };
-        int target = 22;
+        int target = 29;
         System.out.println(solutionUsingThreeLoops(first, second, target));
     }
 
     private static List<Integer> solutionUsingThreeLoops(int[] first, int[] second, int target) {
-        List<Integer> output = new ArrayList<>();
 
-        while (output.isEmpty()){
-            for (int i = 0; i < first.length; i++) {
-                for (int j = 0; j < second.length; j++) {
-                    if (first[i] + second[j] == target) {
-                        output.addAll(List.of(first[i], second[j]));
-                        return output;
-                    }
-                }
-            }
-            if(target != 0)
-                target --;
+        while (true){
+            for (int i = 0; i < first.length; i++)
+                for (int j = 0; j < second.length; j++)
+                    if (first[i] + second[j] == target)
+                        return List.of(first[i], second[j]);
+            target --;
         }
-        return null;
     }
 }
---
+++ SumOfSubArray.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class SumOfSubArray {
+    public static void main(String[] args) {
+        int[] num =  {0, 5, -7, 1, -4, 7, 6, 1, 4, 1, 10};
+        int target = -3;
+
+        solutionUsingTwoForLoop(num,target);
+        solutionUsingWhileLoop(num,target);
+    }
+
+    private static void solutionUsingWhileLoop(int[] num, int target) {
+        int start=0, end= num.length;
+        while (start < num.length -1){
+            if(end < start){
+                start ++;
+                end = num.length;
+            }
+            int[] subArrays = Arrays.copyOfRange(num, start, end);
+            int sum = Arrays.stream(subArrays).sum();
+            if(sum == target)
+                System.out.println(Arrays.toString(subArrays));
+            end --;
+        }
+    }
+
+    private static void solutionUsingTwoForLoop(int[] num, int target) {
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i+1; j < num.length+1; j++) {
+                int[] subArray = Arrays.copyOfRange(num, i, j);
+                int sum = Arrays.stream(subArray).sum();
+                if(sum == target)
+                    System.out.println(Arrays.toString(subArray));
+            }
+        }
+    }
+}
